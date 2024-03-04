0c6690e9d9baa06a56b447bc8e057bc5e9f168d7
tiagomnpcosta <132914134+tiagomnpcosta@users.noreply.github.com>
2024-03-04 23:57:01 +0000

    Add files via upload

--- premake5.lua
+++ premake5.lua
@@ -7,15 +7,21 @@ project "GLFW"
 
   files
   {
-  "include/GLFW/glfw3.h",
-  "include/GLFW/glfw3native.h",
+    "include/GLFW/glfw3.h",
+    "include/GLFW/glfw3native.h",
     "src/glfw_config.h",
     "src/context.c",
     "src/init.c",
     "src/input.c",
     "src/monitor.c",
     "src/vulkan.c",
-    "src/window.c"
+    "src/window.c",
+    "src/platform.c",
+    "src/null_init.c",
+    "src/null_monitor.c",
+    "src/null_window.c",
+    "src/null_joystick.c",
+    "src/internal.h"
   }
 
   filter "system:windows"
@@ -33,7 +39,8 @@ project "GLFW"
       "src/win32_window.c",
       "src/wgl_context.c",
       "src/egl_context.c",
-      "src/osmesa_context.c"
+      "src/osmesa_context.c",
+      "src/win32_module.c"
     }
 
     defines 
